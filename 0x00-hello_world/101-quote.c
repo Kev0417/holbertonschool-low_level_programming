ca7fdc9b757c29a14c7d7cded4d563f10b00665b
Kev0417 <1681@holberton.com>
2020-02-05 22:16:50 +0000

    Edit solution for hello_world 9.

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,8 @@
  */
 int main(void)
 {
-int wl = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+int wl;
+wl = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", wl);
 return (1);
 }
