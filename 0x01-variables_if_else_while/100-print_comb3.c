b950b2d679fa3d3b9406786be25cc358286ed76d
Kev0417 <1681@holberton.com>
2020-02-06 22:38:17 +0000

    Add solution for variables 11.

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n1;
+int n2;
+n1 = '0';
+n2 = '0';
+while (n1 <= '8')
+{
+	 while (n2 <= '9')
+	{
+		if (n1 == n2)
+		{
+			n2++;
+		}
+		if (n1 > n2)
+		{
+			n2++;
+			continue;
+		}
+		putchar(n1);
+		putchar(n2);
+		if (n1 == '8' && n2 == '9')
+		{
+			break;
+		}
+		putchar(',');
+		putchar(' ');
+		n2++;
+	}
+	n2 = '0';
+	n1++;
+}
+putchar('\n');
+return (0);
+}
