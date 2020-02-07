deba62fecd4619c1432aca7d4db2d5bed0f0c19d
Kev0417 <1681@holberton.com>
2020-02-07 00:40:09 +0000

    Add solution for variables 13.

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n1, n2, n3, n4;
+n1 = '0';
+n2 = '0';
+n3 = '0';
+n4 = '0';
+while (n1 <= '9')
+	{
+	while (n2 <= '9')
+		{
+		while (n3 <= '9')
+			{
+			while (n4 <= '9')
+				{
+					if (n1 > n3 || n2 > n4)
+						{
+							n4++;
+							continue;
+						}
+					if (n1 == n3 && n2 == n4)
+						{
+							n4++;
+							continue;
+						}
+					putchar(n1);
+					putchar(n2);
+					putchar(' ');
+					putchar(n3);
+					putchar(n4);
+					putchar(',');
+					putchar(' ');
+					n4++;
+				}
+				n4 = '0';
+				n3++;
+			}
+			n3 = '0';
+			n2++;
+		}
+	n2 = '0';
+	n1++;
+	}
+putchar('\n');
+return (0);
+}
