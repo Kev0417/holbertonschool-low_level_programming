3450ddcf224ba472f745435d036d9d3b7c07ec93
Kev0417 <1681@holberton.com>
2020-02-10 22:12:39 +0000

    Add solution for excercise 6.

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * _abs - this is a description :)
+ * @r: character to print
+ * Return: Always 0 (Success)
+ */
+
+int _abs(int r)
+{
+	if (r >= 0)
+		return (r);
+	else
+		return (r * -1);
+}
