7e28268147d6a664ae35647bb729186907613bc7
Kev0417 <1681@holberton.com>
2020-02-10 16:30:42 +0000

    Add solution for exercise 0.

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,2 +1,21 @@
 #include <stdio.h>
 #include "holberton.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	putchar('H');
+	putchar('o');
+	putchar('l');
+	putchar('b');
+	putchar('e');
+	putchar('t');
+	putchar('o');
+	putchar('n');
+	putchar('\n');
+	return (0);
+}
+
