3cbfddd682efd86388a8f3d5e208b2b35b9047b4
Kev0417 <1681@holberton.com>
2020-02-11 15:02:05 +0000

    Add solution for exercise 9.

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+/**
+ * times_table - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void times_table(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			if ((b * a) >= 10)
+			{
+				_putchar((b * a) / 10 + '0');
+				_putchar((b * a) % 10 + '0');
+			}
+			else
+			{
+				_putchar(b * a + '0');
+			}
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
