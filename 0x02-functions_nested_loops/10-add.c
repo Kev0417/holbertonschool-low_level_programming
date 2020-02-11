0e0c609a86170c4fd96f8089e4dbb20f733ef342
Kev0417 <1681@holberton.com>
2020-02-11 15:52:32 +0000

    Add solution for exercise 10.

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * add - check the code for Holberton School students.
+ *@a: char to check
+ *@b: char to check
+ * Return: Always 0.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
