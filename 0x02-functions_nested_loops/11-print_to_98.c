9d884c8aa150678f984c103c2a562c0389565b5f
Kev0417 <1681@holberton.com>
2020-02-11 18:59:28 +0000

    Add solution for exercise 11.

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * print_to_98 - check the code for Holberton School students.
+ * @n: char to check
+ * Return: Always 0.
+ */
+void print_to_98(int n)
+{
+	int x;
+
+	if (n <= 98)
+	{
+		for (x = n; x <= 98; x++)
+		{
+			if (x < 0)
+			{
+				printf("%d", x);
+			}
+			else if (x >= 0)
+			{
+				printf("%d", x);
+			}
+			if (x != 98)
+			{
+				printf(", ");
+			}
+		}
+		printf("\n");
+	} /* if n < 98 close */
+	else
+	{
+		for (x = n; x >= 98; x--)
+		{
+			printf("%d", x);
+			if (x != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+}
+
