aa4bb73abe2f69d85830e313d3cd2e1402c8cc22
Kev0417 <1681@holberton.com>
2020-02-12 04:47:12 +0000

    Add solution for exercise 15.

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - main block
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a, b, c, d;
+	a = 1;
+	b = 2;
+	c = 0;
+
+	while (b < 4000000)
+	{
+		if (b % 2 == 0)
+			c += b;
+
+		d = b;
+		b += a;
+		a = d;
+	}
+	printf("%d\n", c);
+	return (0);
+}
