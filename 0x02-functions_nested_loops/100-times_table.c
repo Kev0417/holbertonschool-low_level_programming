5c34674c31f75273ea647013913221249d47bfde
Kev0417 <1681@holberton.com>
2020-02-12 04:01:29 +0000

    Add solution for exercise 12.

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,49 @@
+#include "holberton.h"
+/**
+ * print_times_table - This is a description :D.
+ *
+ * @n: int type number
+ */
+void print_times_table(int n)
+{
+	int a; /* size */
+	int b; /* multp */
+
+	if (n != 98)
+	{
+		for (a = 0; a <= n; a++)
+		{
+			for (b = 0; b <= n; b++)
+			{
+				if (a * b >= 100)
+				{
+					_putchar(a * b / 100 + '0');
+					_putchar((a * b / 10 % 10) + '0');
+					_putchar(a * b % 10 + '0');
+				}
+				else if (a * b >= 10)
+				{
+					_putchar(' ');
+					_putchar(a * b / 10 + '0');
+					_putchar(a * b % 10 + '0');
+				}
+				else if (a * b <= 9)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(a * b + '0');
+				}
+				else
+					_putchar(a * b + '0');
+
+				if (b != n)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
+
