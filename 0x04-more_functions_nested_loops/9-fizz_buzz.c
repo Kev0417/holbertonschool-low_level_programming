0e67fe654c1aaa372d62de6dd8642542a1bf1c53
Kev0417 <1681@holberton.com>
2020-02-13 20:54:10 +0000

    Add solution for 0x04 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - fizzbuzz challenge.
+ *
+ * Return: numbers
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+		if ((a % 3) == 0 && (a % 5) == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (a % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else if (a % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else
+		{
+			printf("%d", a);
+		}
+		if (a != 100)
+			printf(" ");
+	}
+	printf("\n");
+
+}
