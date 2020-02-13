f05083068d9dbdf55755ef52ce81b05edeffdebe
Kev0417 <1681@holberton.com>
2020-02-13 19:32:37 +0000

    Add solution for 0x04 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * print_diagonal - print a slash at the end of the line..
+ * @n : char to check
+ * Return: numbers
+ */
+
+void print_diagonal(int n)
+{
+	int a, b;
+
+	for (a = 0; a <= n; a++)
+	{
+		if (a != 0)
+		{
+			while (b < a)
+			{
+				_putchar(' ');
+				b++;
+			}
+			_putchar(92);
+			_putchar('\n');
+			b = 0;
+		}
+	}
+}
