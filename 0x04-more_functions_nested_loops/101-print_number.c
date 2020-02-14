8deba16221138517203a10bd36b909a77ab7e3db
Kev0417 <1681@holberton.com>
2020-02-14 04:33:41 +0000

    Edit solution for 0x04 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,7 +1,43 @@
 #include <stdio.h>
 #include "holberton.h"
 /**
- * print_numbers - print numbers from 0 to 9..
- *
- * Return:
+ * print_number - print an integer.
+ * @n: int to check
+ * return :
  */
+
+void print_number(int n)
+{
+	int a, b, c;
+
+	a = n;
+	if (a < 0)
+	{
+		a = a * -1;
+		_putchar('-');
+	}
+
+	b = 1;
+	c = 1;
+	while (c)
+	{
+		if (a / (b * 10) > 0)
+			b = b * 10;
+		else
+			c = 0;
+	}
+
+	while (a >= 0)
+	{
+		if (b == 1)
+		{
+			_putchar(a % 10 + '0');
+			a = -1;
+		}
+		else
+		{
+			_putchar((a / b % 10) + '0');
+			b /= 10;
+		}
+	}
+}
