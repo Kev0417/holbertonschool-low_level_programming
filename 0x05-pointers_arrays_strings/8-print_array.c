e339bdc7998131303494004cd5b582892ee0aafc
Kev0417 <1681@holberton.com>
2020-02-18 22:03:00 +0000

    Add solution for 0x05 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * print_array - print `n` elements of an array of integers
+ * @a: set array
+ * @n: set array size
+ *
+ *
+ *
+ */
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		printf("%d", a[i]);
+		i++;
+		if (i < n)
+			printf(", ");
+	}
+	printf("\n");
+}
