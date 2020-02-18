574ec7c973f084344e69b4c156a87aea52ba9e25
Kev0417 <1681@holberton.com>
2020-02-18 16:20:51 +0000

    Add solution for 0x05 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include <string.h>
+
+/**
+ * _strlen - function that returns the length of a string.
+ * @s : char to check
+ *
+ */
+
+int _strlen(char *s)
+{
+	int a;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+	}
+
+	return (a);
+}
