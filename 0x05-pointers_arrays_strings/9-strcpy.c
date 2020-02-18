4e84dad459f6330aa630f1c0371c554ac05d015b
Kev0417 <1681@holberton.com>
2020-02-18 22:29:13 +0000

    Add solution for 0x05 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * *_strcpy - copy src to dest
+ * @dest: char to check
+ * @src: char to check
+ * Return: dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int a;
+
+	for (a = 0; src[a] != '\0'; a++)
+		dest[i] = src[i];
+
+	dest[i] = '\0';
+
+	return (dest);
+}
