a1cb0c3e8afdcc88035c19d657db690603b60314
Kev0417 <1681@holberton.com>
2020-02-18 17:13:42 +0000

    Add solution for 0x05 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * puts2 - print one char out of 2 of a string
+ * @str: char to check
+ *
+ */
+
+void puts2(char *str)
+{
+	int a;
+
+	for (a = 0; str[a] != '\0'; a++)
+	{
+		if (a % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
