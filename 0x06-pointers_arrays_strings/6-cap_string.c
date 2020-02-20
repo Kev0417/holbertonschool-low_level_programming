70f5ede11a980569a657bd60246819bec85c9056
Kev0417 <1681@holberton.com>
2020-02-20 21:58:51 +0000

    Add solution 0x06 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * cap_string - capitalize all words of a string
+ * @s: char to check
+ * Return: `s`
+ */
+
+char *cap_string(char *s)
+{
+	int a, b;
+	char x[] = {',', '.', ';', '!', '?', '"', '(', ')', '{', '}', 32, 9, 10};
+
+	for (a = 0; s[a] != 0; a++)
+	{
+		for (b = 0; x[b] != 0; b++)
+		{
+			if (s[a] == x[b])
+			{
+				a++;
+				if (s[a] > 96 && s[a] < 123)
+					s[a] -= 32;
+			}
+		}
+	}
+
+	return (s);
+}
+
