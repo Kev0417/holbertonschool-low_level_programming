50a9507f54c78cc75dc707f1ae665cddaffccd69
Kev0417 <1681@holberton.com>
2020-02-25 00:13:26 +0000

    Add solution for 0x07 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _strstr - locate a substring
+ * @haystack: string
+ * @needle: string substring
+ * Return: Pointer to beginning of substring, or NULL if not found.
+ */
+
+char *_strstr(char *h, char *n)
+{
+	int a, b;
+
+	b = 0;
+
+	if (h == NULL || n == NULL)
+		return (0);
+
+	for (; *h != '\0'; h++)
+	{
+		for (a = 0; *(n + a) != '\0'; a++)
+		{
+			if (*h == n[a])
+			{
+				b++;
+			}
+			else if (*h == '\0')
+			{
+				return (NULL);
+			}
+		}
+	}
+}
