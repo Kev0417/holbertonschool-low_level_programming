5bbe5f55edf60d2d94e93ee74b47a237e4afd501
Kev0417 <1681@holberton.com>
2020-02-25 00:20:07 +0000

    Edit solution for 0x07 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -3,32 +3,31 @@
 
 /**
  * _strstr - locate a substring
- * @h: string
- * @n: string substring
+ * @haystack: string
+ * @needle: string substring
  * Return: Pointer to beginning of substring, or NULL if not found.
  */
 
-char *_strstr(char *h, char *n)
+char *_strstr(char *haystack, char *needle)
 {
-	int a, b;
+	int a, b, c;
 
-	b = 0;
-
-	if (h == NULL || n == NULL)
-		return (0);
-
-	for (; *h != '\0'; h++)
+	c = 0;
+	for (a = 0; haystack[a] != '\0'; a++)
 	{
-		for (a = 0; *(n + a) != '\0'; a++)
+		b = 0;
+		while (needle[b + c] != '\0' && haystack[a + c] != '\0'
+				&& needle[b + c] == haystack[a + c])
 		{
-			if (*h == n[a])
-			{
-				b++;
-			}
-			else if (*h == '\0')
-			{
-				return (NULL);
-			}
+			if (haystack[a + c] != needle[b + c])
+				break;
+			c++;
 		}
+		if (needle[b + c] == '\0')
+			return (&haystack[a]);
+		b++;
 	}
+
+	return (NULL);
 }
+
