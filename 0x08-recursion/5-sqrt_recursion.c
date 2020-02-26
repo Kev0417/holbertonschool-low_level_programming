585e29cf89b814a51a8ba7c557bb2a810752aac2
Kev0417 <1681@holbertonschool.com>
2020-02-26 16:25:47 -0500

    Add solution for 0x08 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * _sqrt_recursion - return the natural square root of a number
+ * @n: int number
+ * Return: If no natural square root, return -1. Else return natural
+ * square root
+ */
+int magic_calculator(int x, int y);
+
+int _sqrt_recursion(int n)
+{
+	return magic_calculator(n, 0.5);
+}
+
+/**
+ * magic_calculator - it's magic!
+ * @x: base
+ * @y: power
+ */
+
+int magic_calculator(int x, int y)
+{
+	if (y * y == x)
+		return (y);
+	else if (y * y < x)
+		return (magic_calculator(x, y + 1));
+	else
+		return (-1);
+}
