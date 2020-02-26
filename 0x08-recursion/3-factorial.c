8e1db04e0193d10e6327183e8cb6095ef4ee61b8
Kev0417 <1681@holbertonschool.com>
2020-02-26 15:13:53 -0500

    Add solution for 0x08 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * factorial - return factorial of a given number.
+ * @n: int type number
+ * Return: factorial of number
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	else if (n > 0)
+		return (n * factorial(n - 1));
+	else
+		return (1);
+}
