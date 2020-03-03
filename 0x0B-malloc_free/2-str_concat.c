a0d3e304e2762f83d04663a85657b86b2d8a19ee
Kev0417 <1681@holbertonschool.com>
2020-03-03 14:25:31 -0500

    Edit solution for 0x0B 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -23,7 +23,7 @@ char *str_concat(char *s1, char *s2)
 	while (s2[j] != '\0')
 		j++;
 
-	a = malloc(i + j * sizeof(char) + 2);
+	a = malloc(i + j * sizeof(char) + 1);
 	if (a == NULL)
 		return (NULL);
 
