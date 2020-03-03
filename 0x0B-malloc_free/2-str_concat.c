35fc9b7c44d478745980c5a89178cf507cc9b511
Kev0417 <1681@holbertonschool.com>
2020-03-03 14:36:13 -0500

    Edit solution for 0x0B 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,8 +13,10 @@ char *str_concat(char *s1, char *s2)
 	char *a;
 	int i, j, x, z;
 
-	if (s1 == NULL && s2 == NULL)
-		return (a);
+	if (s1 == NULL)
+		s1 = "";
+	if	(s2 == NULL)
+		s2 = "";
 
 	i = 0;
 	j = 0;
