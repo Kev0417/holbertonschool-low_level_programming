77ad5029e36d3ec4c7e5a4d606014a015c1bf71f
Kev0417 <1681@holbertonschool.com>
2020-03-04 14:39:12 -0500

    Add solution for 0x0B 6

---
+++ 0x0B-malloc_free/100-strtow.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * strtow - slpits a string into words.
+ * @str: string
+ * Return: Null if fail, else return pointer to new string
+ */
+
+char **strtow(char *str)
+{
+
+}
