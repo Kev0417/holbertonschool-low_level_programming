b8e774f7298b36f162940a078a823050f814f701
Kev0417 <1681@holbertonschool.com>
2020-03-05 14:22:02 -0500

    Add solution for 0x0C 3

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * array_range - create an array of integers inclusive of min and max
+ * @min: min value to include
+ * @max: max value to include
+ * Return: pointer to newly created array
+ */
+
+int *array_range(int min, int max)
+{
+	int *s, i;
+
+	s = malloc(sizeof(int) * (max - min) + 1);
+	if (s == 0)
+		return (NULL);
+
+	for (i = 0; min <= max; i++, min++)
+		s[i] = min;
+
+	return (s);
+}
