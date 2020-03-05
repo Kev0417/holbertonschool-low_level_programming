ce9727d30341da620647ea4fcfdf999c1d2e3576
Kev0417 <1681@holbertonschool.com>
2020-03-05 13:49:01 -0500

    Add solution for 0x0C 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates memory for an array given number of elements and size
+ * @nmemb: number of elements
+ * @size: size of each element
+ * Return: Null if error, else pointer to allocated memory
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int i, *s;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	s = malloc(nmemb * size);
+	if (s == 0)
+		return (NULL);
+
+	for (i = 0; i < nmemb; i++)
+		s[i] = 0;
+
+	return (s);
+}
