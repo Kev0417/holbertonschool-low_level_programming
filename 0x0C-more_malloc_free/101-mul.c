6e28ff0273f223a66d40e19dcac7800b1d3ff7c9
Kev0417 <1681@holbertonschool.com>
2020-03-06 09:46:33 -0500

    Edit solution for 0x0C 6

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,6 +2,25 @@
 #include <stdlib.h>
 
 /**
+ * _isNum - checks if it is fully a number
+ * @num: string to check
+ * Return: 1 if all num 0 if not
+ **/
+
+int _isNum(char *num)
+{
+	int i;
+
+	for (i = 0; num[i] != '\0'; i++)
+	{
+		if (num[i] < '0' || num[i] > '9')
+			return (0);
+	}
+	return (1);
+}
+
+
+/**
  * main - multiply 2 numbers passed to main, or Error
  * @argc: argument count
  * @argv: argument vector
@@ -11,6 +30,13 @@
 int main(int argc, char *argv[])
 {
 	(void) argc;
+
+
+	if (argc != 3 || !(_isNum(argv[1])) || !(_isNum(argv[2])))
+	{
+		puts("Error");
+		exit(98);
+	}
 
 	if (argv[1] && argv[2])
 	{
