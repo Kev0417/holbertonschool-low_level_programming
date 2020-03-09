029cf163b11a959358786e2d2bd4ca3a30e49204
Kev0417 <1681@holbertonschool.com>
2020-03-09 16:05:49 -0500

    Add solution 0x0E 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+  * struct dog - Struct type
+  * @name: Pointer
+  * @age: Float
+  * @owner: Pointer
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
