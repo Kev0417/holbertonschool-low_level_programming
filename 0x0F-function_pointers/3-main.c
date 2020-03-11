a535b667c6df08718599a6b74a5dfe3badf2a79b
Kev0417 <1681@holbertonschool.com>
2020-03-11 18:06:24 -0500

    Add solution for 0x0F 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,32 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *
+ *
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, b;
+
+	if (argc < 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if (get_op_func(argv[2]) == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	printf("%i\n", get_op_func(argv[2])(a, b));
+	return (0);
+}
