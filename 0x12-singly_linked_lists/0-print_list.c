fbb5724c7b9c2c63a5e4c776dcc57920dddd29ee
Kev0417 <1681@holbertonschool.com>
2020-03-19 15:59:57 -0500

    Edit solution for 0x12 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,19 +10,18 @@ size_t print_list(const list_t *h)
 {
 	size_t a = 0;
 
-	while(h)
+	while (h)
 	{
 		if (h->str == NULL)
 		{
 			printf("[0] (nill)\n");
-			h = h->next;
 		}
 		else
 		{
 			printf("[%d] %s\n", h->len, h->str);
-			h = h->next;
 			a++;
 		}
+		h = h->next;
 	}
 	return (a);
 }
