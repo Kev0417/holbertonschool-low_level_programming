443ebff2cdc70fa6d8bbd3db93aa71379a12e177
Kev0417 <1681@holbertonschool.com>
2020-03-23 15:31:29 -0500

    Add solution for 0x13 7

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - Get the nth node of a `listint_t` linked list
+ * @head: pointer to head node
+ * @index: index to find in linked list, starting at 0
+ * Return: pointer to node or NULL if failed
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i;
+
+	if (head == NULL)
+		return (NULL);
+
+	for (i = 0; i < index; i++)
+		head = head->next;
+
+	if (head != NULL)
+		return (head);
+	else
+		return (NULL);
+}
+
