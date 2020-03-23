84b60a1bd6e1f3408e84bf4b1ddbd77778911967
Kev0417 <1681@holbertonschool.com>
2020-03-23 11:59:36 -0500

    Add solution for 0x13 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_listint - print all elements of a `listint_t` list
+ * @h: linked list head
+ * Return: number of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	int x = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		x++;
+		h = h->next;
+	}
+	return (x);
+}
