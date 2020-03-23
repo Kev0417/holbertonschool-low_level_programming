2a82926cd46daa60c9ab43b2eb2ed93051ea1688
Kev0417 <1681@holbertonschool.com>
2020-03-23 15:10:41 -0500

    Add solution for 0x13 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+
+/**
+ * free_listint2 - free a `listint_t` list
+ * @head: double pointer to head
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *clear;
+
+	if (head == NULL)
+		return;
+
+	while (*head != NULL)
+	{
+		clear = *head;
+		*head = (*head)->next;
+		free(clear);
+	}
+}
