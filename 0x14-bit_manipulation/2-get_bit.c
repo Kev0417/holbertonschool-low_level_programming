9da9f6fe776b3633f4194377f4e8aed59a650d32
Kev0417 <1681@holbertonschool.com>
2020-03-26 16:31:38 -0500

    Edit  solution for 0x14 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,13 +8,9 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	unsigned long int i;
-
 	if (index > 64)
 		return (-1);
 
-	i = n >> index;
-
-	return (i & 1);
+	return ((n >> index) & 1);
 }
 
