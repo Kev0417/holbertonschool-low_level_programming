355cb726490651bda5376d1a076a0d44a7ecb3aa
Kev0417 <1681@holbertonschool.com>
2020-03-26 13:28:46 -0500

    Add solution for 0x14 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,1 +1,31 @@
+#include "holberton.h"
+
+/**
+ * binary_to_uint - convert a binary number to an unsigned int
+ * @b: char string
+ * Return: converted decimal number or 0 if there is an unconvertable char
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int total, dup;
+	int i;
+
+	if (b == NULL)
+		return (0);
+
+	total = 0;
+	dup = 1;
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+	}
+
+	for (i--; i >= 0; i--, dup *= 2)
+	{
+		if (b[i] == '1')
+			total += dup;
+	}
+	return (total);
+}
 
