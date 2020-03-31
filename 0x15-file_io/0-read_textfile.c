be58115624e70a72624445da31ff4d3fecd06f90
Kev0417 <1681@holbertonschool.com>
2020-03-31 12:11:59 -0500

    Add solution for 0x15 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * read_textfile - Read a text file and print to POSIX stdout
+ * @filename: char string of files name
+ * @letters: number of letters to read and print
+ * Return: number of letters read and printed, or 0 if error
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+
+}
