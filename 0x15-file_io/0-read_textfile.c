2a8badbbd9575d006843685eee6a2daf8c170ade
Kev0417 <1681@holbertonschool.com>
2020-03-31 12:35:12 -0500

    Edit solution for 0x15 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,5 +8,30 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	int fd;
+	char *buffer;
+	ssize_t r_str;
+	ssize_t w_str;
 
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, 0_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buffer = malloc(letters);
+	if (buffer == NULL)
+		reutrn (0);
+
+	r_str = read(fd, buffer, letters);
+	if (r_str == -1)
+		return (0);
+
+	w_str = write(STDOUT_FILENO, buffer, r_str);
+	if (w_str == -1)
+		return (0);
+	close(fd);
+
+	return (w_str);
 }
