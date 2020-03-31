8bd0819b555c1eb51418d88f82965998a0f8077e
Kev0417 <1681@holbertonschool.com>
2020-03-31 13:53:24 -0500

    Add solution for 0x15 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,14 +20,16 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	i = 0;
-	while (text_content[i] != '\0')
-		i++;
+	if (text_content)
+	{
+		i = 0;
+		while (text_content[i] != '\0')
+			i++;
 
-	w_file = write(fd, text_content, i);
-	if (w_file == -1)
-		return (-1);
-
+		w_file = write(fd, text_content, i);
+		if (w_file == -1)
+			return (-1);
+	}
 	close(fd);
 	return (1);
 }
